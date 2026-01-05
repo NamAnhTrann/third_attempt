448d62e11089dde537a9f9367534b7d4d658d357
Nam Anh Tran <steve200994@gmail.com>
2026-01-05 21:19:35 +1100

    a

--- frontend/third_attempt/src/main.ts
+++ frontend/third_attempt/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { register } from 'swiper/element/bundle';
+import { inject } from '@vercel/analytics';
 register();
+inject();
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
