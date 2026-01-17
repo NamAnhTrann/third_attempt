6f5401ab62c8d6c8146a34554822d4af4ba792dd
Nam Anh Tran <steve200994@gmail.com>
2026-01-17 17:34:22 +1100

    vibe

--- frontend/third_attempt/src/app/app.ts
+++ frontend/third_attempt/src/app/app.ts
@@ -124,8 +124,8 @@ export class App {
 
   private defaultPlaylist: string[] = [
     'songs/Another life, another you (Suno AI) - Her.mp3',
+    'songs/song_that_remind_me_of_her.mp3',
   ];
-
   private catPlaylist: string[] = [
     'songs/chevy - uwu.mp3',
     'songs/Nancy Kwai 歸綽嶢 - Teaser (Official Audio).mp3',
