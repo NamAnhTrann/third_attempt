951db7b875d9d02f95169c768fad06f7e3ec5263
Nam Anh Tran <steve200994@gmail.com>
2026-01-19 23:44:52 +1100

    test

--- frontend/third_attempt/src/app/app.ts
+++ frontend/third_attempt/src/app/app.ts
@@ -123,27 +123,29 @@ export class App {
   }
 
   private defaultPlaylist: string[] = [
-         'assets/songs/at-least-were-under-the-same-sky.mp3',
+        'songs/Another life, another you (Suno AI) - Her.mp3',
 
-     'assets/songs/Another life, another you (Suno AI) - Her.mp3',
-
+    'songs/In another life.mp3',
+    'songs/Another life, another you (Suno AI) - Her.mp3',
   ];
   private catPlaylist: string[] = [
-         'assets/songs/Another life, another you (Suno AI) - Her.mp3',
-
-     'assets/songs/at-least-were-under-the-same-sky.mp3',
+    'songs/chevy - uwu.mp3',
+    'songs/Nancy Kwai 歸綽嶢 - Teaser (Official Audio).mp3',
+    'songs/Bye, Summer - IU.mp3',
+    'songs/Original Song- MondaySunday (by Emma) (1).mp3',
+    'songs/can we (feat. Emi Choi).mp3',
   ];
 
   private story_list: string[] = [
-         'assets/songs/Another life, another you (Suno AI) - Her.mp3',
-
-     'assets/songs/at-least-were-under-the-same-sky.mp3',
+    'songs/Philharmonia Orchestra, Laufey - Let You Break My Heart Again (Karaoke Version).mp3',
+    'songs/Love Story (Piano Version).mp3',
+    'songs/Watashino Uso.mp3',
+    'songs/Chainsaw Man The Movie_ Reze Arc  OST -  04 - first glance.mp3',
   ];
 
   private essence_playlist: string[] = [
-         'assets/songs/Another life, another you (Suno AI) - Her.mp3',
-
-     'assets/songs/at-least-were-under-the-same-sky.mp3',
+    'songs/Watashino Uso.mp3',
+    'songs/forward.mp3',
   ];
 
   private currentPlaylist: string[] = [];
--- frontend/third_attempt/src/app/old-content/old-content.ts
+++ frontend/third_attempt/src/app/old-content/old-content.ts
@@ -1,8 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-old-content',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './old-content.html',
   styleUrl: './old-content.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
