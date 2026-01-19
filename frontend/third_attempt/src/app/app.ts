fe2b9fcbacc8f9de5d0ef3fe8df104cd49b899fd
Nam Anh Tran <steve200994@gmail.com>
2026-01-19 23:14:45 +1100

    a

--- frontend/third_attempt/src/app/app.ts
+++ frontend/third_attempt/src/app/app.ts
@@ -123,21 +123,21 @@ export class App {
   }
 
   private defaultPlaylist: string[] = [
-    'songs/At least we’re under the same sky.mp3',
+    'songs/at-least-were-under-the-same-sky.mp3',
     'songs/Another life, another you (Suno AI) - Her.mp3',
   ];
   private catPlaylist: string[] = [
-    'songs/At least we’re under the same sky.mp3',
+    'songs/at-least-were-under-the-same-sky.mp3',
     'songs/Another life, another you (Suno AI) - Her.mp3',
   ];
 
   private story_list: string[] = [
-    'songs/At least we’re under the same sky.mp3',
+    'songs/at-least-were-under-the-same-sky.mp3',
     'songs/Another life, another you (Suno AI) - Her.mp3',
   ];
 
   private essence_playlist: string[] = [
-    'songs/At least we’re under the same sky.mp3',
+    'songs/at-least-were-under-the-same-sky.mp3',
     'songs/Another life, another you (Suno AI) - Her.mp3',
   ];
 
