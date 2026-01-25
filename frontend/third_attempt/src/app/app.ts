ba39ef2c7ee5eb81e963e27483657fde9c9c0389
Nam Anh Tran <steve200994@gmail.com>
2026-01-25 20:52:28 +1100

    a

--- frontend/third_attempt/src/app/app.ts
+++ frontend/third_attempt/src/app/app.ts
@@ -58,18 +58,17 @@ export class App {
         this.startLoading();
       }
 
-   if (event instanceof NavigationEnd) {
-  const currentPath = this.router.url.split('#')[0];
-  const nextPath = event.urlAfterRedirects.split('#')[0];
+      if (event instanceof NavigationEnd) {
+        const currentPath = this.router.url.split('#')[0];
+        const nextPath = event.urlAfterRedirects.split('#')[0];
 
-  // Ignore hash-only navigation
-  if (currentPath !== nextPath) {
-    this.handleRouteMusic(event.urlAfterRedirects);
-  }
+        // Ignore hash-only navigation
+        if (currentPath !== nextPath) {
+          this.handleRouteMusic(event.urlAfterRedirects);
+        }
 
-  this.stopLoading();
-}
-
+        this.stopLoading();
+      }
 
       if (
         event instanceof NavigationCancel ||
@@ -140,7 +139,10 @@ export class App {
   }
 
   private defaultPlaylist: string[] = [
-    'songs/kanegi. - closer to you (official lyric video).mp3',
+    'songs/melancholy.mp3 (1).mp3',
+    'songs/Love Story (Piano Version).mp3',
+
+
     // 'songs/at-least-were-under-the-same-sky-v3.mp3.mp3',
   ];
   private catPlaylist: string[] = [
