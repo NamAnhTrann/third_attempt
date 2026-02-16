aca685b29c0e6b68f8360887d4c901c8fa5e6e4d
Vercel <vercel[bot]@users.noreply.github.com>
2026-02-16 07:57:25 +0000

    Set up Vercel Web Analytics for your project
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- frontend/third_attempt/src/main.ts
+++ frontend/third_attempt/src/main.ts
@@ -2,8 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { register } from 'swiper/element/bundle';
-import { inject } from "@vercel/analytics"
+import { inject } from '@vercel/analytics';
+
+// Initialize Swiper custom elements
 register();
+
+// Initialize Vercel Web Analytics
 inject();
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
